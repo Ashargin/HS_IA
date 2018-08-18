776f9bdf6a2cad125578a17ed0284c67a62ac130
Loic <loic.omnes@free.fr>
2018-08-19 00:59:48 +0200

    v2 architecture ready for subfuncs

--- v2.0_UNSTABLE.py
+++ v2.0_UNSTABLE.py
@@ -1,5 +1,6 @@
 import sys
 import numpy as np
+import copy
 
 turn = 0
 my_deck = []
@@ -20,10 +21,12 @@ draft_vals = {1: 60, 2: 50, 3: 55, 4: 55, 5: 50, 6: 55, 7: 65, 8: 60, 9: 65, 10:
  76, 138: 59, 139: 82, 140: 48, 141: 39, 142: 35, 143: 5, 144: 45, 145: 47, 146:
  42, 147: 45, 148: 55, 149: 48, 150: 60, 151: 81, 152: 87, 153: 44, 154: 51, 
 155: 51, 156: 44, 157: 35, 158: 63, 159: 58, 160: 39}
-min_turn_strength = 8
-max_turn_strength = 20
+min_turn_strength = 4
+max_turn_strength = 26
 curve = {2: 6, 3: 4, 4: 4, 5: 3, 6: 3, 7: 4}
 cost_strength = 5
+over_cost_punish_threshold = 2
+over_cost_punish_strength = 1
 draw_target = 7
 draw_strength = 2
 spells_target = 6
@@ -31,65 +34,180 @@ spells_strength = 3
 duplicate_strength = 8
 cost_advantage = 22
 
-def draft_score(card, deck): # update score : abilities, items
-    #atk = card['atk']
-    #cost = card['cost']
-    #card_type = card['type']
-    #if card_type == 0:
-        #return 10 * (atk - 1.5 * cost)
-        # return 50 + 10 * (atk + hp - 2 * cost) + 3 * (hp - atk)
-    #else:
-        #return -1000
-
+def draft_score(card, deck):
     card_type = card['type']
     turn_strength = 0
     if turn >= min_turn_strength - 1:
         turn_strength = min((turn - min_turn_strength + 1) / (max_turn_strength - min_turn_strength), 1)
+
     cost = card['cost']
     cost_bonus = 0
-    if cost > 1 and card_type == 0:
+    if card_type == 0:
         cost_reached = 0
         cost_target = 0
+        this_cost_strength = cost_strength
+        if cost_reached >= cost_target * turn / 30 + over_cost_punish_threshold:
+            this_cost_strength += cost_strength * over_cost_punish_strength * (cost_reached - cost_target * turn / 30 - over_cost_punish_threshold)
         if cost >= 7:
             cost_reached = len([c for c in deck if c['cost'] >= 7 and c['type'] == 0])
             cost_target = curve[7]
+        elif cost <= 2:
+            cost_reached = len([c for c in deck if c['cost'] <= 2 and c['type'] == 0])
+            cost_target = curve[2]
         else:
             cost_reached = len([c for c in deck if c['cost'] == cost and c['type'] == 0])
             cost_target = curve[cost]
-        cost_bonus = cost_strength * turn_strength * (cost_target * turn / 30 - cost_reached)
+        cost_bonus = this_cost_strength * turn_strength * (cost_target * turn / 30 - cost_reached)
+
     draw = card['draw']
     draw_reached = sum([c['draw'] for c in deck])
     draw_bonus = draw_strength * turn_strength * draw * (draw_target * turn / 30 - draw_reached)
+
     spell_bonus = 0
     if card_type > 0:
         spells_reached = len([c for c in deck if c['type'] > 0])
         spell_bonus = spells_strength * turn_strength * (spells_target * turn / 30 - spells_reached)
+
     duplicate_bonus = 0
     if card_type > 0:
         card_id = card['id']
         duplicates = len([c for c in deck if c['id'] == card_id])
         duplicate_bonus = -duplicate_strength * duplicates
+
     val = draft_vals[card['id']]
-    print('val', val, file=sys.stderr)
-    print('cost', cost_bonus, file=sys.stderr)
-    print('draw', draw_bonus, file=sys.stderr)
-    print('spell', spell_bonus, file=sys.stderr)
-    print('duplicates', duplicate_bonus, file=sys.stderr)
     return val + cost_bonus + draw_bonus + spell_bonus + duplicate_bonus
 
-def battle_score(card, mana): # update score : abilities, items
+def play_score(card, mana, my_board, op_board):
     card_score = draft_vals[card['id']]
     cost = card['cost']
-    # card_score = 50 + 10 * (atk + hp - 2 * cost) + 3 * (hp - atk)
+    card_type = card['type']
+
     if cost < mana:
         card_score -= cost_advantage * (mana - cost)
-    elif cost > mana:
-        card_score -= 1000
-    return card_score
 
-# def compute_plays
+    target = 0
+    if card_type == 1:
+        target_pos = np.argmax([c['atk'] for c in my_board])
+        target = my_board[target_pos]['instance']
 
-# def compute_attacks
+    elif card_type == 2:
+        target_pos = np.argmax([c['atk'] for c in op_board])
+        target = op_board[target_pos]['instance']
+
+    elif card_type == 3:
+        if op_board:
+            target_pos = np.argmax([c['atk'] for c in op_board])
+            target = op_board[target_pos]['instance']
+        else:
+            target = -1
+
+    return card_score, target
+
+def compute_plays(my_hand, my_board, op_board, can_attack, mana):
+    can_play = True
+    plays = []
+    targets = []
+
+    while can_play:
+        playable_0 = [card for card in my_hand if card['cost'] <= mana and card['type'] == 0]
+        if len(my_board) == 6:
+            playable_0 = []
+        playable_1 = [card for card in my_hand if card['cost'] <= mana and card['type'] == 1]
+        if not my_board:
+            playable_1 = []
+        playable_2 = [card for card in my_hand if card['cost'] <= mana and card['type'] == 2]
+        if not op_board:
+            playable_2 = []
+        playable_3 = [card for card in my_hand if card['cost'] <= mana and card['type'] == 3]
+        playable = copy.copy(playable_0 + playable_1 + playable_2 + playable_3)
+
+        if not playable:
+            can_play = False
+        else:
+            round_scores, round_targets = zip(*[play_score(card, mana, my_board, op_board) for card in playable])
+            round_play_pos = np.argmax(round_scores)
+            round_play_card = playable[round_play_pos]
+            round_play = round_play_card['instance']
+            cost = round_play_card['cost']
+            card_type = round_play_card['type']
+            round_target = round_targets[round_play_pos]
+
+            hand_pos = np.argwhere(np.array([c['instance'] for c in my_hand]) == round_play)[0][0]
+            my_hand.pop(hand_pos)
+            if card_type == 0:
+                my_board.append(copy.copy(round_play_card))
+            elif card_type > 1 and round_target != -1:
+                target_pos = np.argwhere(np.array([c['instance'] for c in op_board]) == round_target)[0][0]
+                target_card = op_board[target_pos]
+                if 'W' in target_card['abilities']:
+                    target_card['abilities'] = target_card['abilities'].replace('W', '-')
+                else:
+                    target_card['hp'] += round_play_card['hp']
+                if target_card['hp'] <= 0:
+                    op_board.pop(target_pos)
+            mana -= cost
+    
+            plays.append(round_play)
+            targets.append(round_target)
+
+    return plays, targets
+
+def compute_attacks(can_attack, op_board):
+    attackers = []
+    targets = []
+    for card in can_attack:
+        attacker = card['instance']
+        attackers.append(attacker)
+        if card['atk'] > 0:
+            guard = False
+            guard_pos = 0
+            for i, op_card in enumerate(op_board):
+                if 'G' in op_card['abilities']:
+                    guard = True
+                    guard_pos = i
+            if not guard:
+                targets.append(-1)
+            else:
+                op_card = op_board[guard_pos]
+                attacked = op_card['instance']
+                targets.append(attacked)
+                if 'W' in op_card['abilities']:
+                    op_card['abilities'] = op_card['abilities'].replace('W', '-')
+                else:
+                    if 'L' in card['abilities']:
+                        op_card['hp'] = 0
+                    else:
+                        op_card['hp'] -= card['atk']
+                    if op_card['hp'] <= 0:
+                        op_board.pop(guard_pos)
+
+    return attackers, targets
+
+    # attackers, targets = compute_attacks # compute variables
+    # for i, attacker in enumerate(attackers):
+    #     target = targets[i]
+    #     actions.append('ATTACK ' + str(attacker) + ' ' + str(target))
+
+    #       attacker_pos = np.argwhere(np.array([c['instance'] for c in can_attack]) == attacker)[0][0]
+    #     attacker_card = can_attack[attacker_pos]
+    #     if target == -1:
+    #         atk = attacker_card['atk']
+    #         op_hp -= atk
+    #         if 'D' in attacker_card['abilities']:
+    #             my_hp += atk
+
+    #       else:
+    #         target_pos = np.argwhere(np.array([c['instance'] for c in op_board]) == target)[0][0]
+    #         target_card = op_board[target_pos]
+    #         attacker_abilities = attacker_card['abilities']
+    #         target_abilities = target_card['abilities']
+    #         if 'W' in attacker_abilities and 'W' in target_abilities:
+    #             attacker_card['abilities'] = attacker_abilities.replace('W', '-') # 0 atk?
+    #             target_card['abilities'] = target_abilities.replace('W', '-')
+
+    #           elif 'W' in attacker_abilities:
+
+# compute my_hp, my_mana, my_rune?, op_hp, op_rune?, my_board, op_board, my_hand, can_attack
 
 while True: # update my_rune, op_rune?
 
@@ -121,62 +239,92 @@ while True: # update my_rune, op_rune?
             my_board.append(card)
         else:
             op_board.append(card)
+    can_attack = copy.copy(my_board)
 
     if turn < 30: # draft phase
         scores = [draft_score(card, my_deck) for card in my_hand]
-        print(scores, file=sys.stderr)
         pick = np.argmax(scores)
         print('PICK ' + str(pick))
         my_deck.append(my_hand[pick])
 
     else: # battle phase
         actions = []
-        can_play = True #play phase
+
         played = False
-        my_board_count = len(my_board)
-        while can_play:
-            playable = [card for card in my_hand if card['cost'] <= my_mana and card['type'] == 0]
-            if playable and my_board_count < 6:
-                scores = [battle_score(card, my_mana) for card in my_hand]
-                play = np.argmax(scores)
-                card = my_hand[play]
-                actions.append('SUMMON ' + str(card['instance']))
-                my_hp += card['my_hp_change']
-                my_mana -= card['cost']
-                op_hp += card['op_hp_change']
-                if 'C' in card['abilities']:
-                    my_board.append(card)
-                my_board_count += 1
-                my_hand.pop(play)
-                played = True
+        plays, targets = compute_plays(copy.copy(my_hand), copy.copy(my_board), copy.copy(op_board), copy.copy(can_attack), my_mana) # play phase
+
+        for i in range(len(plays)):
+            play = plays[i]
+            target = targets[i]
+            play_pos = np.argwhere(np.array([c['instance'] for c in my_hand]) == play)[0][0]
+            card = my_hand[play_pos]
+            card_type = card['type']
+
+            if card_type == 0:
+                actions.append('SUMMON ' + str(play))
             else:
-                can_play = False
+                actions.append('USE ' + str(play) + ' ' + str(target))
+
+            my_hp += card['my_hp_change']
+            my_mana -= card['cost']
+            op_hp += card['op_hp_change']
+            my_hand.pop(play_pos)
+
+            if card_type == 0:
+                my_board.append(copy.copy(card))
+                if 'C' in card['abilities']:
+                    can_attack.append(copy.copy(card))
+
+            elif card_type == 1:
+                for board in [can_attack, my_board]:
+                    try:
+                        target_pos = np.argwhere(np.array([c['instance'] for c in board]) == target)[0][0]
+                        target_card = board[target_pos]
+                        target_card['atk'] += card['atk']
+                        target_card['hp'] += card['hp']
+                        for i, v in enumerate(card['abilities']):
+                            if v != '-':
+                                target_card['abilities'] = target_card['abilities'][:i] + v + target_card['abilities'][i+1:]
+                        if 'C' in target_card['abilities'] and target not in [c['instance'] for c in can_attack]:
+                            can_attack.append(copy.copy(target_card))
+                    except IndexError:
+                        pass
+
+            elif card_type == 2:
+                target_pos = np.argwhere(np.array([c['instance'] for c in op_board]) == target)[0][0]
+                target_card = op_board[target_pos]
+                target_card['atk'] += card['atk']
+                for i, v in enumerate(card['abilities']):
+                    if v != '-':
+                        target_card['abilities'] = target_card['abilities'].replace(v, '-')
+                if 'W' in target_card['abilities']:
+                    target_card['abilities'] = target_card['abilities'].replace('W', '-')
+                else:
+                    target_card['hp'] += card['hp']
+                if target_card['hp'] <= 0:
+                    op_board.pop(target_pos)
+
+            else:
+                if target != -1:
+                    target_pos = np.argwhere(np.array([c['instance'] for c in op_board]) == target)[0][0]
+                    target_card = op_board[target_pos]
+                    if 'W' in target_card['abilities']:
+                        target_card['abilities'] = target_card['abilities'].replace('W', '-')
+                    else:
+                        target_card['hp'] += card['hp']
+                    if target_card['hp'] <= 0:
+                        op_board.pop(target_pos)
+                else:
+                    op_hp += card['hp']
+
+            played = True
 
         attacked = False
-        # attacks = compute_attacks
-        for my_card in my_board: # attack phase # compute variables
-            if my_card['atk'] > 0:
-                attacked = True
-                guard = False
-                guard_pos = 0
-                for i, op_card in enumerate(op_board):
-                    if 'G' in op_card['abilities']:
-                        guard = True
-                        guard_pos = i
-                if not guard:
-                    actions.append('ATTACK ' + str(my_card['instance']) + ' -1')
-                else:
-                    op_card = op_board[guard_pos]
-                    actions.append('ATTACK ' + str(my_card['instance']) + ' ' + str(op_card['instance']))
-                    if 'W' in op_card['abilities']:
-                        op_card['abilities'] = op_card['abilities'].replace('W', '-')
-                    else:
-                        if 'L' in my_card['abilities']:
-                            op_card['hp'] = 0
-                        else:
-                            op_card['hp'] -= my_card['atk']
-                        if op_card['hp'] <= 0:
-                            op_board.pop(guard_pos)
+        attackers, targets = compute_attacks(can_attack, op_board) # attack phase
+
+        for i in range(len(attackers)):
+            actions.append('ATTACK ' + str(attackers[i]) + ' ' + str(targets[i]))
+            attacked = True
 
         if not played and not attacked:
             actions = ['PASS']
